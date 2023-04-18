26493e8d1d6c05905c71601c2cc678f9e30a1108
hgparkk <phg2559@naver.com>
2023-04-18 12:53:54 +0900

    new commit

---
+++ meetingMatching/meetingMatching.c
@@ -0,0 +1,104 @@
+﻿/*
+프로그램 내용 : 큐를 이용한 미팅주선 프로그램
+실습일 : 2023.04.18
+실습자 : 박현규
+학번 : 202111678
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_QUEUE_SIZE 100
+
+typedef struct Student
+{
+	char name[80];
+	char gender;
+}Element;
+
+typedef struct CircularQueue {
+	Element data[MAX_QUEUE_SIZE];
+	int front;
+	int rear;
+}Queue;
+
+void error(char* str)
+{
+	printf("%s\n", str);
+	exit(1);
+}
+
+void init_queue(Queue* q) { q->front = q->rear = 0; }
+int is_empty(Queue* q) { return q->front == q->rear; }
+int is_full(Queue* q) { return q->front == (q->rear + 1) % MAX_QUEUE_SIZE; }
+int size(Queue* q) { return(q->rear - q->front + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; }
+
+void enqueue(Queue* q,Element val)
+{
+	if (is_full(q))
+		error(" 큐 포화 에러");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = val;
+}
+
+Element dequeue(Queue* q)
+{
+	if (is_empty(q))
+		error(" 큐 공백 에러");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+Element peek(Queue* q)
+{
+	if (is_empty(q))
+		error(" 큐 공백 에러");
+	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+void print_queue(Queue* q, char msg[])
+{
+	int i, maxi = q->rear;
+	if (q->front >= q->rear) maxi += MAX_QUEUE_SIZE;
+	printf("%s[%2d]=", msg, size(q));
+	for (i = q->front + 1;i <= maxi;i++)
+		printf("%s, %c", q->data[i % MAX_QUEUE_SIZE].name, q->data[i%MAX_QUEUE_SIZE].gender);
+	printf("\n");
+}
+
+void main()
+{
+	Element e;
+	char name[80];
+	char gender;
+	Queue mq;
+	Queue fq;
+	init_queue(&mq);
+	init_queue(&fq);
+	while(1)
+	{
+		printf("고객의 이름은?(종료는 -1) >>");
+		scanf("%s", name);
+		printf("\n");
+		if (strcmp(name, '-1') == 0)
+		{
+			printf("미팅 주선 프로그램을 종료합니다.");
+			break;
+		}
+		else
+		{
+			printf("성별을 입력(남자는 m, 여자는 f) >>");
+			scanf("%c", &gender);
+			if (strcmp('m', gender))
+			{
+				strcpy(e.name,name);
+				e.gender = gender;
+				if (is_empty(&fq))
+				{
+					printf("아직 대상자가 없습니다.");
+					enqueue(&mq,e);
+				}
+			}
+		}
+	}
+}
